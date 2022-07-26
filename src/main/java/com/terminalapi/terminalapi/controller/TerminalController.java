4873d8351dff0a796708ec8f4cd9d68bdbb85c05
Ademola Adebayo <ademola.adebayo@etranzact.com>
2022-07-26 11:52:30 +0100

    Minor chnages was done

--- src/main/java/com/terminalapi/terminalapi/controller/TerminalController.java
+++ src/main/java/com/terminalapi/terminalapi/controller/TerminalController.java
@@ -1,44 +1,29 @@
 package com.terminalapi.terminalapi.controller;
 
+import com.terminalapi.terminalapi.entity.Terminal;
 import com.terminalapi.terminalapi.model.TerminalModel;
 import com.terminalapi.terminalapi.response.BaseResponse;
 import com.terminalapi.terminalapi.service.TerminalService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.ZoneId;
+import java.util.List;
 
 @RestController
+@RequestMapping("api/terminal-request")
 public class TerminalController {
-    private final TerminalService terminalService;
     @Autowired
-    public TerminalController(TerminalService terminalService) {
-        this.terminalService = terminalService;
+    TerminalService terminalService;
+
+    @PostMapping
+    public Terminal postRequest(@RequestBody TerminalModel terminalModel){
+        return terminalService.addNewTerminal(terminalModel);
     }
 
-    @PostMapping("/terminalresponse")
-    public ResponseEntity<?> terminalResponse(@RequestBody TerminalModel terminalModel){
-        if(terminalService.addNewTerminal(terminalModel) != null){
-            BaseResponse baseResponse = new BaseResponse();
-            long dateTrans = terminalModel.getTransDate().atStartOfDay(ZoneId.systemDefault()).toEpochSecond() * 1000;
-            long datePos = terminalModel.getPosDate().atStartOfDay(ZoneId.systemDefault()).toEpochSecond() * 1000;
-
-            baseResponse.setTerminalId(terminalModel.getTerminalId());
-            baseResponse.setAmount(terminalModel.getAmount());
-            baseResponse.setResponse_code(terminalModel.getResponse_code());
-            baseResponse.setResponseMessage(terminalModel.getResponseMessage());
-            baseResponse.setRrn(terminalModel.getRrn());
-            baseResponse.setStan(terminalModel.getStan());
-            baseResponse.setSerialNumber(terminalModel.getSerialNumber());
-            baseResponse.setPosDate(datePos);
-            baseResponse.setTransDate(dateTrans);
-
-            return ResponseEntity.ok(baseResponse);
-        }
-        return ResponseEntity.status(500).body("Terminal already exist");
+    @GetMapping
+    public List<Terminal> getRequest(){
+        return terminalService.getTerminal();
     }
 }
--- src/main/java/com/terminalapi/terminalapi/service/TerminalService.java
+++ src/main/java/com/terminalapi/terminalapi/service/TerminalService.java
@@ -6,6 +6,8 @@ import com.terminalapi.terminalapi.repository.TerminalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class TerminalService {
     private final TerminalRepository terminalRepository;
@@ -15,28 +17,23 @@ public class TerminalService {
         this.terminalRepository = terminalRepository;
     }
 
-    public Terminal getTerminal(TerminalModel terminalModel) {
-        return (Terminal) terminalRepository.findAll();
+    public List<Terminal> getTerminal() {
+        return  terminalRepository.findAll();
 
     }
 
     public Terminal addNewTerminal(TerminalModel terminalModel) {
-        Terminal terminalOptional = terminalRepository.findByTerminalId(terminalModel.getTerminalId());
         Terminal terminal = new Terminal();
-        if (terminalOptional == null) {
-            terminal.setTerminalId(terminalModel.getTerminalId());
-            terminal.setAmount(terminalModel.getAmount());
-            terminal.setPosDate(terminalModel.getPosDate());
-            terminal.setResponse_code(terminalModel.getResponse_code());
-            terminal.setResponseMessage(terminalModel.getResponseMessage());
-            terminal.setRrn(terminalModel.getRrn());
-            terminal.setTransDate(terminalModel.getTransDate());
-            terminal.setStan(terminalModel.getStan());
-            terminal.setSerialNumber(terminalModel.getSerialNumber());
-            terminalRepository.save(terminal);
-            return terminal;
-        } else {
-            return null;
-        }
+        terminal.setTerminalId(terminalModel.getTerminalId());
+        terminal.setAmount(terminalModel.getAmount());
+        terminal.setPosDate(terminalModel.getPosDate());
+        terminal.setResponse_code(terminalModel.getResponse_code());
+        terminal.setResponseMessage(terminalModel.getResponseMessage());
+        terminal.setRrn(terminalModel.getRrn());
+        terminal.setTransDate(terminalModel.getTransDate());
+        terminal.setStan(terminalModel.getStan());
+        terminal.setSerialNumber(terminalModel.getSerialNumber());
+        terminalRepository.save(terminal);
+        return terminal;
     }
 }
