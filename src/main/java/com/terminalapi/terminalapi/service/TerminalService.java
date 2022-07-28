775b9fa02ffe9dbfc494a82047120d73fe920037
David Alimazoya <David.Alimazoya@etranzact.com>
2022-07-28 12:32:54 +0100

    prune the code to just save request

--- src/main/java/com/terminalapi/terminalapi/service/TerminalService.java
+++ src/main/java/com/terminalapi/terminalapi/service/TerminalService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TerminalService {
@@ -23,9 +24,9 @@ public class TerminalService {
     }
 
     public Terminal addNewTerminal(TerminalModel terminalModel) {
-        Terminal terminalOptional = terminalRepository.findBySerialNumber(terminalModel.getSerialNumber());
+
         Terminal terminal = new Terminal();
-        if (terminalOptional == null) {
+
             terminal.setTerminalId(terminalModel.getTerminalId());
             terminal.setAmount(terminalModel.getAmount());
             terminal.setPosDate(terminalModel.getPosDate());
@@ -39,24 +40,7 @@ public class TerminalService {
             terminal.setPmNumber(terminalModel.getPmNumber());
             terminal.setNoticeReceiver(terminalModel.getNoticeReceiver());
             terminal.setTransType(terminalModel.getTransType());
-            terminalRepository.save(terminal);
-            return terminal;
-        } else {
-            terminal.setTerminalId(terminalModel.getTerminalId());
-            terminal.setAmount(terminalModel.getAmount());
-            terminal.setPosDate(terminalModel.getPosDate());
-            terminal.setResponse_code("01");
-            terminal.setResponseMessage("TRANSACTION FAILED");
-            terminal.setRrn(terminalModel.getRrn());
-            terminal.setTransDate(terminalModel.getTransDate());
-            terminal.setStan(terminalModel.getStan());
-            terminal.setSerialNumber(terminalModel.getSerialNumber());
-            terminal.setMaskPan(terminalModel.getMaskPan());
-            terminal.setPmNumber(terminalModel.getPmNumber());
-            terminal.setNoticeReceiver(terminalModel.getNoticeReceiver());
-            terminal.setTransType(terminalModel.getTransType());
-            terminalRepository.save(terminal);
-            return null;
-        }
+
+           return terminalRepository.save(terminal);
     }
 }
